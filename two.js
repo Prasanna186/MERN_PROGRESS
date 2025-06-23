8345628ca3998a02eb29dfd2a816a590cf572829
Prasanna Kundurthi <144698908+Prasanna186@users.noreply.github.com>
2025-06-23 18:49:00 +0530

    Add files via upload

---
+++ two.js
@@ -0,0 +1,24 @@
+const Student = {
+    fullName : "Prasanna Kundurthi",
+    age : 20,
+    cgpa : 9.03,
+    isGraduated : false
+
+};
+console.log(Student);
+console.log(typeof Student);
+console.log(Student["fullName"]);
+console.log(Student.age);
+let a = 15;
+let b = 11;
+console.log("a + b = " + (a + b));
+console.log("a - b = " + (a - b));
+console.log("a * b = " + (a * b));
+console.log("a / b = " + (a / b));
+console.log("a % b = " + (a % b));
+console.log("a ** b = " + (a ** b));
+console.log("a = ", a , "b = ", b);
+a++;
+console.log("a++ = ", a);
+++b;
+console.log("++b = ", b);
